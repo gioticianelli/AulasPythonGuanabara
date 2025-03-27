adc58caef53a8d649c85310e0eae0b1fd5d35880
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-27 11:52:37 -0300

    Aprendi os conceitos de int, float, str e bool

---
+++ Aula 6.py
@@ -0,0 +1,15 @@
+n1 = int(input('Digite o 1° número: '))
+n2 = int(input('Digite o 2° número: '))
+soma = (n1 + n2)
+
+#print('A soma entre o valor', n1, 'e', n2, 'é: ', soma)
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
+
+x = bool(input('Digite um valor: ')) #bolenao mostra somenete True ou False
+print(x)
+
+y = int(input('Digite um valor: '))
+print(y)
+
+n = float(input('Digite um valor: '))
+print(n)
