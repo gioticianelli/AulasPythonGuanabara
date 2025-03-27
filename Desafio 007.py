5637e83415b6f78582c522c9dc023c71ef7a7494
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-27 13:09:44 -0300

    Média Aritmética

---
+++ Desafio 007.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+n3 = float(input('Digite sua terceira nota: '))
+
+media = (n1 + n2 + n3) / 3
+
+print('Sua média final é: {:.2f}'.format(media)) # :.2f considera somente os dois valores depois da vírgula
+
