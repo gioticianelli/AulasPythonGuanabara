8374dafc232e347c293da56db519bca4dd1cc345
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-27 12:00:35 -0300

    Desafios

---
+++ Desafio 003.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite outro valor: '))
+n2 = float(input('Digite mais um valor: '))
+n3 = float(input('Digite o ultimo valor: '))
+soma = n1 + n2 + n3
+print('A soma entre {}, {} e {} é: {}'.format(n1, n2, n3, soma))
---
+++ Desafio 004.py
@@ -0,0 +1,8 @@
+x = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(x))
+print('Só tem espaços? ', x.isspace())
+print('É um número? ', x.isnumeric())
+print('É alfabetico? ', x.isalpha())
+print('Está em maiúsculas? ', x.isupper())
+print('Está em minúsculas? ', x.islower())
+print('Está capitalizada? ', x.istitle())
