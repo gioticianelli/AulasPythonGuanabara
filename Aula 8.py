c9fb69d1d715ba0572fbe250fa487362ef420396
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-29 10:40:09 -0300

    Utilizando módulos

---
+++ Aula 8.py
@@ -0,0 +1,16 @@
+import math
+num = int(input('Digite um núemro: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num,(raiz)))
+
+
+import random
+num = random.randint(1, 100)  # gera um número aleatorio de 1 a 100
+print(num)
+
+# import ctrl + espaço mostra todas as bibliotecas
+
+import emoji
+print(emoji.emojize('Python is :thumbs_up:'))   #https://www.webfx.com/tools/emoji-cheat-sheet/
+print(emoji.emojize('Texto teste :purple_heart:'))
+
