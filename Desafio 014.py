24dc7854803a4fe63252fe79b9275797f128f435
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-29 10:00:05 -0300

    Conversor de Temperaturas

---
+++ Desafio 014.py
@@ -0,0 +1,5 @@
+celcius = float(input('Qual a temperatura da sua região em graus celcius? '))
+
+fahrenheit = (celcius * 1.8) + 32
+
+print('A temperatura {}°C corresponde a {}°F'.format(celcius, fahrenheit))
