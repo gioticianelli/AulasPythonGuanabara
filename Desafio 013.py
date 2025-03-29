832639a40c51650f094e6fa3360aaab9df00f4e2
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-29 09:54:56 -0300

    Reajuste Salarial

---
+++ Desafio 013.py
@@ -0,0 +1,6 @@
+salario = float(input('Qual o valor do salário do funcionário? '))
+
+novo_salario = salario + (salario * 15 / 100)
+
+print('O novo salário será com 15% de aumento.\n'
+      'O novo valor è de {}'.format(novo_salario))
