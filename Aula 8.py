5f0a344a96b7741c24cba291547fd3023e03171e
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-03-29 10:57:53 -0300

    Módulos

--- Aula 8.py
+++ Aula 8.py
@@ -14,3 +14,4 @@ import emoji
 print(emoji.emojize('Python is :thumbs_up:'))   #https://www.webfx.com/tools/emoji-cheat-sheet/
 print(emoji.emojize('Texto teste :purple_heart:'))
 
+# https://docs.python.org/pt-br/3.13/library/math.html
