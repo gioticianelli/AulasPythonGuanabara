2115a6b88e483eb0dec9eff60e5700fc31a7a153
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-04-08 20:10:37 -0300

    Desafio

---
+++ Desafio 23.py
@@ -0,0 +1,9 @@
+num = input('Digite um número: ')
+
+
+
+print('Analisando o número {}...'.format(num))
+print('Unidade: {}'.format)
+print('Dezena: {}'.format)
+print('Centena: {}'.format)
+print('Milhar: {}'.format)
