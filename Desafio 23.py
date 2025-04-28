6c7da18e3ed8169db0f6e36afc187d6dfffa0132
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-04-28 14:25:30 -0300

    Desafio 23. Unidade, decena, centena e milhar

--- Desafio 23.py
+++ Desafio 23.py
@@ -1,9 +1,12 @@
-num = input('Digite um número: ')
+num = int(input('Digite um número: '))
 
-
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
 
 print('Analisando o número {}...'.format(num))
-print('Unidade: {}'.format)
-print('Dezena: {}'.format)
-print('Centena: {}'.format)
-print('Milhar: {}'.format)
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
