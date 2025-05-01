01ce417cea78da1b5e744f39588ef5c2a93e5ca6
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-05-01 16:23:09 -0300

    Verifica se o o ano é bissexto ou não.

---
+++ 03 Condições em Python (if..else)/Desafio 32.py
@@ -0,0 +1,7 @@
+#Exercício Python 32: Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
+
+ano = int(input('Será que é bissexto? Digite o ano: '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO!'.format(ano))
+else:
+    print('O ano {} NÃO É BISSEXTO!'.format(ano))
