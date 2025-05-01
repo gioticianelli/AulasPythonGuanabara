2efa47960448ceec8530913f1f0a1c77cdb91d5f
Giovana Ticianelli <ticianelligiovana@gmail.com>
2025-05-01 15:05:01 -0300

    Tocando um MP3

--- 02 Usando módulos de python/Desafio 21.py
+++ 02 Usando módulos de python/Desafio 21.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('UmSonho.mp3')
+pygame.mixer.music.load('../UmSonho.mp3')
 pygame.mixer.music.play()
 input()                                                 # É necessário colocar input() na nova versão
 pygame.event.wait()
